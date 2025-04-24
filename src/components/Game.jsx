802b2414471d356f5436f94ac7df270d32b792f0
KRanggaR <krangar83540@gmail.com>
2025-04-24 14:50:28 +0530

    Successfully loaded 5 pokemon details from API

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Game() {
-
-// fetching pokemons
-const fetchPokemons = async ()=> {
-    try{
-    const response =  await fetch('https://pokeapi.co/api/v2/pokemon?limit=50&offset=0')
-    const data = await response.json();
-    console.log(data)
+function getRandomIds(length){
+    const max=1000;
+    const ids = new Set();
+    while(ids.size < length) {
+        const randomId = Math.floor(Math.random()*max);
+        ids.add(randomId);
     }
+    return Array.from(ids); 
+}
+function Game({length=5}) {
+    const [data, setData] = useState([]);
     
-    catch(err) {
-    console.error(err)
-    
-  };
+    const fetchPokemonData = async () => {
+        try {
+            const ids = getRandomIds(length);
+
+            const detailedData = await Promise.all(
+                ids.map(async (id) => {
+                    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+                    const pokemon_details = await res.json();
+
+                    return {
+                        id: id,
+                        name: pokemon_details.species.name,
+                        image: pokemon_details.sprites.other["official-artwork"].front_default,
+                    };
+                })
+            );
+            setData(detailedData);
+            console.log(detailedData)
+        }
+
+        catch (err) {
+            console.error(err.message);
+        };
+
+    }
+    return (
+        <>
+            <button onClick={fetchPokemonData}>FetchPokemons</button>
+        </>
+    )
 }
 
-  return (
-    <>
-    <button onClick={fetchPokemons}>FetchPokemons</button>  
-    </>
-  )
-}
-
-export default Game
+export default Game;
---
+++ src/components/reducer/pokemon.jsx
@@ -0,0 +1,13 @@
+import React, {useReducer} from 'react'
+
+function pokemon() {
+    const [state, dispatch] = useReducer(reducerFunction, initialState);
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default pokemon
