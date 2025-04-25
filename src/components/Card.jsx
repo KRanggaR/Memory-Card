d44966d2ff356a78186e9560b70202d9d5e2c4e2
KRanggaR <krangar83540@gmail.com>
2025-04-25 17:43:10 +0530

    Added card for pokemons UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import '../styles/card.css';
 
-function Card() {
+function Card({ pokemon, AddPokemon }) {
+console.log(pokemon.name, pokemon.id)
   return (
+
     <>
       
+
+        {pokemon && (
+          <div className='card-container' onClick={()=>AddPokemon(pokemon.id)}>
+            <div className='image-block'>
+              <img className='pokemon-image' src={pokemon.image} alt="" />
+            </div>
+            <div className='name-block'>
+              <p className='pokemon-name'>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
+            </div>
+          </div>
+    )}
+
     </>
+      // {console.log('This is data from card', data)}
   )
 }
 
