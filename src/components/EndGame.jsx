7dc6702a90e8e6297c11c1db9e9c0c4f84fcbf0f
KRanggaR <krangar83540@gmail.com>
2025-04-28 15:42:20 +0530

    Corrected image path

--- src/components/EndGame.jsx
+++ src/components/EndGame.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import '../styles/endgame.css';
-import sadImage from '/assets/sad.png';  
-import happyImage from '/assets/happy.png';  
 
 export default function EndGame({ state: { currentScore, gameEnded, pokemons, error }, resetGame }) {
   return (
@@ -12,7 +10,7 @@ export default function EndGame({ state: { currentScore, gameEnded, pokemons, er
             <div className="end-game-box">
               <h2>{error}</h2>
               <img className='end-game-image' 
-                src={sadImage}
+                src={"/Memory-Card/assets/sad.png"} 
                 alt="Sad"  
               />
               <button className='end-game-button' onClick={resetGame}>Try Again?</button>
@@ -24,7 +22,7 @@ export default function EndGame({ state: { currentScore, gameEnded, pokemons, er
             <div className="end-game-box">
               <h2>{currentScore === pokemons.length ? "You Won" : "You Lost"}</h2>
               <img className='end-game-image' 
-                src={currentScore === pokemons.length ? happyImage : sadImage} 
+                src={currentScore === pokemons.length ? "/Memory-Card/assets/happy.png" : "/Memory-Card/assets/sad.png"} 
                 alt={currentScore === pokemons.length ? "Happy" : "Sad"}  
               />
               <p>Score : {currentScore}</p>
